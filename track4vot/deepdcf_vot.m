0d725a769d600a1d7327027ec51aaf2146acb914
Qiang Wang <wangqiang2015@ia.ac.cn>
2016-11-25 20:57:21 +0800

    gpuSupport_vot

--- track4vot/deepdcf_vot.m
+++ track4vot/deepdcf_vot.m
@@ -48,52 +48,64 @@ handle.quit(handle);
 end
 
 function [state, location] = deepdcf_initialize(I, region, varargin)
+state.gpu = ispc();
+state.visual = true;
 
 net = load('simplenn_vgg_deepdcfnet.mat');
-state.net = vl_simplenn_tidy(net.net) ;
-
+net = vl_simplenn_tidy(net.net);
+if state.gpu    %gpuSupport
+    net = vl_simplenn_move(net, 'gpu');
+end
+state.net = net;
 
 state.lambda = 1e-4;
-state.padding = 1.5;
+state.padding = 1.2;
 state.output_sigma_factor = 0.1; 
 state.interp_factor = 0.02;
 state.output_sigma = sqrt(prod([50,50]))*state.output_sigma_factor;
 
 state.yf = fft2(gaussian_shaped_labels(state.output_sigma, state.net.meta.normalization.imageSize(1:2)));
+if state.gpu,state.yf = gpuArray(state.yf);end    %gpuSupport
 
 state.pos = floor(region([2,1])+region([4,3])/2);
 state.target_sz = region([4,3]);
 state.window_sz = floor(state.target_sz*(1+state.padding));
 
-patch = get_subwindow(I, state.pos, state.window_sz);
+patch = single(imresize(get_subwindow(I, state.pos, state.window_sz),...
+    state.net.meta.normalization.imageSize(1:2)));
+if state.gpu,patch= gpuArray(patch);end    %gpuSupport
 
-target = bsxfun(@minus,...
-    single(imresize(patch,state.net.meta.normalization.imageSize(1:2))),...
-    state.net.meta.normalization.averageImage);
-res = vl_simplenn(state.net, target,[],[],'mode','test','conserveMemory',true);
+target = bsxfun(@minus,patch,state.net.meta.normalization.averageImage);
+res = vl_simplenn(state.net,target,[],[],'mode','test','conserveMemory',true);
 xf = fft2(res(end).x);
 
-kf = linear_correlation(xf, xf);
+kf = sum(xf.*conj(xf),3)/numel(xf);
 state.model_alphaf = state.yf ./ (kf + state.lambda);
 state.model_xf = xf;
 
 location = region;
-imshow(uint8(I));
-rectangle('Position',location);
+if state.visual
+subplot(1,2,1);imshow(patch);
+subplot(1,2,2);imshow(uint8(I));
+rectangle('Position',location,'EdgeColor','g');
+drawnow;
+end
 
 end
 
 function [state, location] = deepdcf_update(state, I, varargin)
 
-patch = get_subwindow(I, state.pos, state.window_sz);
+patch_crop = single(imresize(get_subwindow(I, state.pos, state.window_sz),...
+    state.net.meta.normalization.imageSize(1:2)));
 
-search = bsxfun(@minus,...
-    single(imresize(patch,state.net.meta.normalization.imageSize(1:2))),...
-    state.net.meta.normalization.averageImage);
+if state.gpu,patch_crop= gpuArray(patch_crop);end    %gpuSupport
+
+search = bsxfun(@minus,patch_crop,state.net.meta.normalization.averageImage);
+
 res = vl_simplenn(state.net, search,[],[],'mode','test','conserveMemory',true);
 zf = fft2(res(end).x);
 
-kzf = linear_correlation(zf, state.model_xf);
+kzf = sum(zf.*conj(state.model_xf),3)/numel(zf);
 
 response = real(ifft2(state.model_alphaf .* kzf));
 [vert_delta, horiz_delta] = find(response == max(response(:)), 1);
@@ -106,14 +118,16 @@ end
 state.pos = state.pos + [vert_delta - 1, horiz_delta - 1].*...
     state.window_sz./state.net.meta.normalization.imageSize(1:2);
 
-patch = get_subwindow(I, state.pos, state.window_sz);
-target = bsxfun(@minus,...
-    single(imresize(patch,state.net.meta.normalization.imageSize(1:2))),...
-    state.net.meta.normalization.averageImage);
+patch = single(imresize(get_subwindow(I, state.pos, state.window_sz),...
+    state.net.meta.normalization.imageSize(1:2)));
+
+if state.gpu,patch= gpuArray(patch);end    %gpuSupport
+
+target = bsxfun(@minus,patch,state.net.meta.normalization.averageImage);
 
 res = vl_simplenn(state.net, target,[],[],'mode','test','conserveMemory',true);
 xf = fft2(res(end).x);
-kf = linear_correlation(xf, xf);
+kf = sum(xf.*conj(xf),3)/numel(xf);
 alphaf = state.yf ./ (kf + state.lambda);   %equation for fast training
 
 state.model_alphaf = (1 - state.interp_factor) * state.model_alphaf + state.interp_factor * alphaf;
@@ -121,14 +135,26 @@ state.model_xf = (1 - state.interp_factor) * state.model_xf + state.interp_facto
 
 box = [state.pos([2,1]) - state.target_sz([2,1])/2, state.target_sz([2,1])];
 
-location = double(box);
-imshow(uint8(I));
-rectangle('Position',location);
-drawnow
+location = double(gather(box));
+
+if state.visual
+subplot(1,2,1);im_show_add_response(patch_crop,response);
+subplot(1,2,2);imshow(uint8(I));
+rectangle('Position',location,'EdgeColor','g');
+drawnow;
+end
 
 end
 
 
+function im_show_add_response(im,response)
+sz = size(response);
+response = circshift(response, floor(sz(1:2) / 2) - 1);
+
+imshow(uint8(gather(im)));hold on;
+h = imagesc(response);colormap(jet);
+set(h,'AlphaData',gather(response)+0.35);
+end
 
 
 
--- track4vot/linear_correlation.m
+++
@@ -1,3 +0,0 @@
-function kf = linear_correlation(xf, yf)
-kf = sum(xf .* conj(yf), 3) / numel(xf);
-end
