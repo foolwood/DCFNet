6b01d3f80a77b6eb785027f66c46bbd5cf6bd4f5
Qiang Wang <wangqiang2015@ia.ac.cn>
2017-02-13 11:14:47 +0800

    memory friendly

--- train_New/train_cnn_dcf.m
+++ train_New/train_cnn_dcf.m
@@ -3,18 +3,18 @@ function [net, info] = train_cnn_dcf(varargin)
 run('vl_setupnn.m') ;
 fftw('planner','patient');
 opts.dataset = 3;
-opts.networkType = 5;
+opts.networkType = 7;
 opts.lossType = 1;
 opts.expDir = fullfile('../data',...
     ['dataset-',num2str(opts.dataset),'-net-',num2str(opts.networkType),'-loss-' num2str(opts.lossType) '-DCFNet-New']) ;
-opts.imdbPath = fullfile(opts.expDir, 'imdb.mat');
+opts.imdbPath = fullfile('../data',['dataset-',num2str(opts.dataset)], 'imdb.mat');
 opts.lite = false;
 
 trainOpts.learningRate = 1e-5;
 trainOpts.momentum = 0.9;
 trainOpts.weightDecay = 0.0005;
 trainOpts.gpus = [2];
-trainOpts.numEpochs = 20;
+trainOpts.numEpochs = 50;
 trainOpts.batchSize = 16;
 opts.train = trainOpts;
 
